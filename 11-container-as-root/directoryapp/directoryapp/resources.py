552627f7feadf4955b294b1acf8dfc86f59b7429
Kamon <kamon.ayeva@gmail.com>
2013-04-08 22:50:10 +0200

    Next step: The Directory object is the Root

---
+++ 11-container-as-root/directoryapp/directoryapp/resources.py
@@ -0,0 +1,120 @@
+import colander
+import deform.widget
+
+from persistent import Persistent
+
+from zope.interface import (
+    implementer,
+    )
+
+from pyramid.security import (
+    Allow,
+    Everyone,
+    )
+    
+from substanced.content import content
+from substanced.property import PropertySheet
+from substanced.root import Root
+from substanced.schema import (
+    Schema,
+    NameSchemaNode
+    )
+from substanced.util import renamer
+
+        
+## The Content object PropertySheet definition,
+## based on the content schema.
+class ContactSchema(Schema):
+    name = NameSchemaNode(
+        editing=lambda c, r: r.registry.content.istype(c, 'Contact'),
+        )
+    firstname= colander.SchemaNode(
+        colander.String(),
+        )
+    lastname= colander.SchemaNode(
+        colander.String(),
+        )
+    email= colander.SchemaNode(
+        colander.String(),
+        )
+    telephone= colander.SchemaNode(
+        colander.String(),
+        )
+    bio = colander.SchemaNode(
+        colander.String(),
+        widget=deform.widget.RichTextWidget()
+        )
+
+class ContactPropertySheet(PropertySheet):
+    schema = ContactSchema()
+
+
+## The Content object (aka Model) class 
+# Type registration
+@content(
+    'Contact',
+    icon='icon-align-left',
+    add_view='add_contact', 
+    propertysheets = (
+        ('Basic', ContactPropertySheet),
+        ),
+    )
+class Contact(Persistent):
+
+    name = renamer()
+    
+    def __init__(self, firstname='', lastname='', email='', telephone='', bio=''):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.telephone = telephone
+        self.bio = bio
+
+
+
+## Directory object, Root of the "Substance D" instance
+
+class IDirectory(IFolder):
+    pass
+
+
+class DirectorySchema(Schema):
+    """ The schema representing the directory root. """
+
+    title = colander.SchemaNode(
+        colander.String(),
+        missing=''
+        )
+    description = colander.SchemaNode(
+        colander.String(),
+        missing=''
+        )
+    
+class DirectoryPropertySheet(PropertySheet):
+    schema = DirectorySchema()
+    
+@content(
+    'Root',
+    icon='icon-home',
+    propertysheets = (
+        ('', DirectoryPropertySheet),
+        ),
+    after_create= ('after_create', 'after_create2')
+    )
+@implementer(IDirectory)
+class Directory(Root):
+    title = ''
+    description = ''
+
+    @property
+    def sdi_title(self):
+        return self.title
+
+    @sdi_title.setter
+    def sdi_title(self, value):
+        self.title = value
+    
+    def after_create2(self, inst, registry):
+        acl = getattr(self, '__acl__', [])
+        acl.append((Allow, Everyone, 'view'))
+        self.__acl__ = acl
---
+++ 11-container-as-root/directoryapp/directoryapp/views.py
@@ -0,0 +1,101 @@
+from pyramid.httpexceptions import HTTPFound
+from pyramid.view import view_config
+from pyramid.renderers import get_renderer
+
+from substanced.sdi import mgmt_view
+from substanced.form import FormView
+from substanced.interfaces import IRoot
+from substanced.interfaces import IFolder
+
+from .resources import (
+    ContactSchema,
+    Contact,
+
+    IDirectory,
+    DirectorySchema,
+    Directory,
+    )
+
+
+## SDI "add" view for contacts
+@mgmt_view(
+    context=IDirectory,
+    name='add_contact',
+    tab_title='Add Contact', 
+    permission='sdi.add-content', 
+    renderer='substanced.sdi:templates/form.pt',
+    tab_condition=False,
+    )
+class AddContactView(FormView):
+    title = 'Add Contact'
+    schema = ContactSchema()
+    buttons = ('add',)
+
+    def add_success(self, appstruct):
+        registry = self.request.registry
+        name = appstruct.pop('name')
+        contact = registry.content.create('Contact', **appstruct)
+        self.context[name] = contact
+        return HTTPFound(
+            self.request.sdiapi.mgmt_path(self.context, '@@contents')
+            )
+
+
+## Retail view for contacts
+@view_config(
+    context=Contact,
+    renderer='templates/contact.pt',
+    )
+def contact_view(context, request):
+    return {'title': context.firstname + ' ' + context.lastname,
+            'body': context.bio,
+            'master': get_renderer('templates/master.pt').implementation(),
+           }
+
+
+## THis snippet is now REMOVED since 
+## the Directory is the root, and therefor not addable anymore... (add this to the index.rst...)
+## SDI "add" view for directory instances
+# @mgmt_view(
+#     context=IRoot,
+#     name='add_directory',
+#     tab_title='Add Directory', 
+#     permission='sdi.add-content', 
+#     renderer='substanced.sdi:templates/form.pt',
+#     tab_condition=False,
+#     )
+# class AddDirectoryView(FormView):
+#     title = 'Add Directory'
+#     schema = DirectorySchema()
+#     buttons = ('add',)
+# 
+#     def add_success(self, appstruct):
+#         registry = self.request.registry
+#         name = appstruct.pop('name')
+#         directory = registry.content.create('Directory', **appstruct)
+#         self.context[name] = directory
+#         return HTTPFound(
+#             self.request.sdiapi.mgmt_path(self.context, '@@contents')
+#             )
+
+
+## Retail view for directory instances
+@view_config(
+    context=Directory,
+    renderer='templates/directory.pt',
+    )
+def directory_view(context, request):
+    contacts = []
+    for name, contact in context.items():
+        if request.registry.content.istype(contact, 'Contact'):
+            contacts.append(
+                {'url': resource_url(contact, request),
+                 'fullname': contact.firstname + ' ' + contact.lastname,
+                 })
+
+    return {'title': context.title,
+            'description': context.description,
+            'contacts': contacts,
+            'master': get_renderer('templates/master.pt').implementation(),
+           }
+
